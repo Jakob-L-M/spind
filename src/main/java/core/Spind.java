89972666f4dd5ddd139f8d12a06ccbf7edd9f179
Jakob L. Müller <me@jakob-l-m.de>
2024-02-16 09:52:33 +0100

    added method to properly open input files

--- src/main/java/core/Spind.java
+++ src/main/java/core/Spind.java
@@ -1,12 +1,18 @@
 package core;
 
+import io.RelationalInput;
 import runner.Config;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Spind {
     Config config;
 
-    public void execute() {
-
+    public void execute() throws IOException {
+        List<RelationalInput> inputs = openInputs();
         // 1) get all unary attributes.
         // 2) create unary candidates.
 
@@ -18,5 +24,23 @@ public class Spind {
         // 3.4) Generate new candidates for next layer.
 
         // 4) Save the output
+    }
+
+    private List<RelationalInput> openInputs() throws IOException {
+
+        List<RelationalInput> inputs = new ArrayList<>();
+
+        int relationOffset = 0;
+        for (int relationId = 0; relationId < config.tableNames.length; relationId++) {
+            inputs.add(new RelationalInput(config.tableNames[relationId],
+                    config.folderPath + File.separator + config.databaseName + File.separator + config.tableNames[relationId] + config.fileEnding,
+                    config,
+                    relationOffset,
+                    relationId
+                    )
+            );
+            relationOffset += inputs.getLast().attributes.size();
+        }
+        return inputs;
     }
 }
