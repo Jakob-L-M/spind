35d9f080a62ad395d9e0e8302395de7a700d6333
Jakob L. Müller <me@jakob-l-m.de>
2024-02-16 11:56:18 +0100

    initial version of Validator methods

--- src/main/java/io/Validator.java
+++ src/main/java/io/Validator.java
@@ -3,43 +3,59 @@ package io;
 import runner.Config;
 import structures.Attribute;
 import structures.Candidates;
+import structures.Entry;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class Validator {
     Config config;
     Attribute[] attributeIndex;
     Candidates candidates;
-    Queue<String> topValues;
-    Reader[] readers;
+    Queue<Entry> topValues;
+    BufferedReader[] readers;
+    List<Integer> readersInTopValues;
 
     public Validator(Config config, Attribute[] attributeIndex, Candidates candidates) throws IOException {
         this.config = config;
         this.attributeIndex = attributeIndex;
         this.candidates = candidates;
+        this.readersInTopValues = new ArrayList<>();
         topValues = new PriorityQueue<>();
         initReaders(attributeIndex);
     }
 
-    public void validate() {
-
+    public void validate() throws IOException {
+        HashMap<Integer, Long> valueGroup = loadNextGroup();
+        while (valueGroup != null) {
+            candidates.prune(valueGroup);
+            updateTopValues();
+            valueGroup = loadNextGroup();
+        }
     }
 
-    public void loadNextGroup() {
-
+    private void updateTopValues() throws IOException {
+        for (int reader : readersInTopValues) {
+            updateReader(reader);
+        }
+        readersInTopValues.clear();
     }
 
-    public void parseLine() {
+    public HashMap<Integer, Long> loadNextGroup() {
+        if (topValues.isEmpty()) return null;
 
+        Entry firstEntry = topValues.poll();
+        readersInTopValues.add(firstEntry.getReaderNumber());
+        HashMap<Integer, Long> valueGroup = firstEntry.getConnectedAttributes();
+        while (topValues.peek() != null && topValues.peek().equals(firstEntry)) {
+            readersInTopValues.add(topValues.peek().getReaderNumber());
+            valueGroup.putAll(topValues.poll().getConnectedAttributes()); // can not be null!
+        }
+        return valueGroup;
     }
 
     private void initReaders(Attribute[] attributeIndex) throws IOException {
@@ -47,6 +63,13 @@ public class Validator {
         readers = new BufferedReader[relations.size()];
         for (int i = 0; i < readers.length; i++) {
             readers[i] = Files.newBufferedReader(Path.of(config.tempFolder + File.separator + "relation_" + relations.get(i) + ".txt"));
+            updateReader(i);
         }
+    }
+
+    private void updateReader(int readerNumber) throws IOException {
+        String nextLine = readers[readerNumber].readLine();
+        if (nextLine == null) return;
+        topValues.add(new Entry(nextLine, readerNumber));
     }
 }
