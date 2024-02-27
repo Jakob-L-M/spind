0943b1e0c1d04ff6c4f0e94763fc43bd68653217
Jakob L. Müller <me@jakob-l-m.de>
2024-02-27 22:35:26 +0100

    multithreaded merging

--- src/main/java/core/Spind.java
+++ src/main/java/core/Spind.java
@@ -20,22 +20,26 @@ import java.util.List;
 
 public class Spind {
     Config config;
-    int[] relationOffsets;
     Logger logger;
     Output output;
     Clock clock;
     RelationMetadata[] relationMetadata;
+    int mergeSize = 25;
+    int sortSize = 300_000;
+    int chunkSize = 500_000;
 
-    public Spind(Config config) throws IOException {
+    public Spind(Config config) {
         this.config = config;
         this.output = new Output(config.resultFolder);
         this.clock = new Clock();
         this.logger = LoggerFactory.getLogger(Spind.class);
         clock.start("total");
-        this.relationMetadata = initializeRelations();
     }
 
     public void execute() throws IOException {
+
+        clock.start("init");
+        this.relationMetadata = initializeRelations(chunkSize);
 
         // 1) init the helper Classes
         Candidates candidates = new Candidates();
@@ -43,6 +47,8 @@ public class Spind {
         // 1) get all unary attributes.
         Attribute[] attributes = buildUnaryAttributes();
         candidates.loadUnary(attributes);
+
+        logger.info("Finished initialization. Took: " + clock.stop("init") + "ms");
 
         // 3) while attributes not empty.
         while (attributes.length > 0) {
@@ -52,42 +58,71 @@ public class Spind {
             attachAttributes(attributes);
             List<SortJob> sortJobs = createSortJobs();
 
-
-            logger.info(" Starting layer: " + candidates.layer + " with " + attributes.length + " attributes forming " + candidates.current.keySet().stream().mapToInt(x -> candidates.current.get(x).size()).sum());
+            logger.info("Starting layer: " + candidates.layer + " with " + attributes.length + " attributes forming " + candidates.current.keySet().stream().mapToInt(x -> candidates.current.get(x).size()).sum() + " candidates");
             // 3.1) Load all attributes of the candidates.
+            clock.start("sorting");
             List<MergeJob> mergeJobs = sortJobs.parallelStream().map(sortJob -> {
-                        Sorter sorter = new Sorter(200_000);
+                        Sorter sorter = new Sorter(sortSize);
                         return sorter.process(sortJob, config);
                     }
             ).toList();
+            logger.info("Finished sorting. Took: " + clock.stop("sorting") + "ms");
 
-            List<MergeJob> groupedMergeJobs = new ArrayList<>();
-            for (int i = 0; i < relationMetadata.length; i++) {
-                groupedMergeJobs.add(new MergeJob(new ArrayList<>(), i, false));
-            }
-
-            for (MergeJob mergeJob : mergeJobs) {
-                groupedMergeJobs.get(mergeJob.relationId()).chunkPaths().addAll(mergeJob.chunkPaths());
-            }
-
-
-            Attribute[] finalAttributes = attributes;
-            groupedMergeJobs.parallelStream().forEach(mergeJob -> {
-                if (mergeJob.chunkPaths().isEmpty()) {
-                    return;
+            clock.start("merging");
+            while (!mergeJobs.isEmpty()) {
+                // 1) group by relation
+                List<MergeJob> groupedMergeJobs = new ArrayList<>();
+                for (int i = 0; i < relationMetadata.length; i++) {
+                    groupedMergeJobs.add(new MergeJob(new ArrayList<>(), i, null));
                 }
-                Merger merger = new Merger();
-                Path resultPath = Path.of(config.tempFolder + File.separator + "relation_" + mergeJob.relationId() + ".txt");
-                merger.merge(mergeJob.chunkPaths(), resultPath, finalAttributes);
-            });
+                for (MergeJob mergeJob : mergeJobs) {
+                    groupedMergeJobs.get(mergeJob.relationId()).chunkPaths().addAll(mergeJob.chunkPaths());
+                }
 
+                // split jobs and save next parts
+                List<MergeJob> nextJobs = new ArrayList<>();
+                List<MergeJob> currentJobs = new ArrayList<>();
+
+                for (MergeJob job : groupedMergeJobs) {
+                    if (job.chunkPaths().isEmpty()) {
+                        continue;
+                    }
+                    int n = job.chunkPaths().size();
+                    if (n >= mergeSize) {
+                        List<Path> nextPaths = new ArrayList<>();
+                        for (int i = 0; i < n; i += mergeSize) {
+                            Path resultPath = Path.of(job.chunkPaths().get(i) + "_m_" + i + ".txt");
+                            currentJobs.add(new MergeJob(new ArrayList<>(job.chunkPaths().subList(i, Math.min(i+ mergeSize,n))), job.relationId(), resultPath));
+                            nextPaths.add(resultPath);
+                        }
+                        nextJobs.add(new MergeJob(nextPaths, job.relationId(), null));
+                    }
+                    else {
+                        Path resultPath = Path.of(config.tempFolder + File.separator + "relation_" + job.relationId() + ".txt");
+                        currentJobs.add(new MergeJob(job.chunkPaths(), job.relationId(), resultPath));
+                    }
+                }
+
+                Attribute[] finalAttributes = attributes;
+                currentJobs.parallelStream().forEach(mergeJob -> {
+                    if (mergeJob.chunkPaths().isEmpty()) {
+                        return;
+                    }
+                    Merger merger = new Merger();
+                    merger.merge(mergeJob.chunkPaths(), mergeJob.to(), finalAttributes);
+                });
+                mergeJobs = nextJobs;
+            }
+            logger.info("Finished merging. Took: " + clock.stop("merging") + "ms");
 
             // 3.2) Validate candidates.
+            clock.start("validation");
             Validator validator = new Validator(config, attributes, candidates);
             validator.validate();
 
             // remove all dependant candidates, that do not reference any attribute
             candidates.cleanCandidates();
+            logger.info("Finished validation. Took: " + clock.stop("validation") + "ms");
 
             int unary = candidates.current.keySet().stream().mapToInt(x -> candidates.current.get(x).size()).sum();
             logger.info("Found " + unary + " pINDs at level " + candidates.layer);
@@ -96,7 +131,9 @@ public class Spind {
             // 3.3) Clean up files.
 
             // 3.4) Generate new attributes for next layer.
+            clock.start("generateNext");
             attributes = candidates.generateNextLayer(attributes, relationMetadata);
+            logger.info("Finished generating next layer. Took: " + clock.stop("generateNext") + "ms");
         }
 
         // clean chunks
@@ -110,7 +147,7 @@ public class Spind {
         output.storeMetadata(config, clock);
     }
 
-    private RelationMetadata[] initializeRelations() throws IOException {
+    private RelationMetadata[] initializeRelations(int chunkSize) throws IOException {
         RelationMetadata[] relationMetadata = new RelationMetadata[config.tableNames.length];
 
         int relationOffset = 0;
@@ -118,7 +155,7 @@ public class Spind {
             relationMetadata[relationId] = new RelationMetadata(
                     config.tableNames[relationId],
                     relationId,
-                    250_000,
+                    chunkSize,
                     relationOffset,
                     Path.of(config.folderPath + File.separator + config.databaseName + File.separator + config.tableNames[relationId] + config.fileEnding),
                     config
