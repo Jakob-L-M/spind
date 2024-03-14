e8b3964da74e401f76e4ee29ddde767d7866ad63
Jakob L. Müller <me@jakob-l-m.de>
2024-03-14 22:45:32 +0100

    added docs

--- src/main/java/io/Merger.java
+++ src/main/java/io/Merger.java
@@ -5,24 +5,35 @@ import structures.Entry;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
+/**
+ * Class to handel file merging.
+ */
 public class Merger {
     private PriorityQueue<Entry> headValues;
     private BufferedReader[] readers;
 
+    /**
+     * Attaches the readers and load the first value.
+     *
+     * @param files The List of paths to files which should be merged.
+     * @throws IOException if a files could not be found.
+     */
     private void init(List<Path> files) throws IOException {
         this.headValues = new PriorityQueue<>(files.size());
         this.readers = new BufferedReader[files.size()];
 
         for (int index = 0; index < files.size(); ++index) {
-            BufferedReader reader = Files.newBufferedReader(files.get(index));
+            BufferedReader reader = new BufferedReader(new FileReader(files.get(index).toString()), 2 ^ 16);
             this.readers[index] = reader;
             String firstLine = reader.readLine();
             if (firstLine != null) {
@@ -71,11 +82,18 @@ public class Merger {
         }
     }
 
+    /**
+     * Updates the reader which is connected to the current entry.
+     *
+     * @param current the entry which was last handled
+     * @throws IOException If the reader is unable to read the next two lines
+     */
     private void updateHeadValues(Entry current) throws IOException {
         int readerNumber = current.getReaderNumber();
         String nextLine = this.readers[readerNumber].readLine();
 
         if (nextLine != null) {
+            // if there is one more line, there have to be two more lines.
             this.headValues.add(new Entry(nextLine, this.readers[readerNumber].readLine(), readerNumber));
         }
     }
@@ -84,6 +102,7 @@ public class Merger {
         output.write(previous.getValue());
         output.newLine(); // delimiter between value and connected attributes
 
+        // Case 1: There is only
         if (containedAttributes.isEmpty()) {
             output.write(previous.getSerializedAttributes());
             if (isFinal) {
@@ -95,37 +114,40 @@ public class Merger {
                 }
             }
         } else {
-
-            // there are multiple attribute in the group
+            // Case 2: there are multiple attributes from multiple files in the group
             previous.load(); // add the last member
             previous.getConnectedAttributes().forEach((k, v) -> containedAttributes.merge(k, v, Long::sum));
             for (Integer attribute : containedAttributes.keySet()) {
                 long occurrences = containedAttributes.get(attribute);
 
-                output.write(String.valueOf(attribute));
-                output.write(','); // attribute-occurrence separator
-                output.write(String.valueOf(occurrences));
-                output.write(';'); // attribute-attribute separator
+                writeAttribute(output, attribute, occurrences);
 
                 if (isFinal) {
                     attributes[attribute].getMetadata().totalValues += occurrences;
                     attributes[attribute].getMetadata().uniqueValues++;
                 }
             }
+            containedAttributes.clear();
         }
-        containedAttributes.clear();
 
         output.newLine();
     }
 
+    private void writeAttribute(BufferedWriter output, int key, long occurrences) throws IOException {
+        output.write(String.valueOf(key));
+        output.write(','); // attribute-occurrence separator
+        output.write(String.valueOf(occurrences));
+        output.write(';'); // attribute-attribute separator
+    }
+
+    /**
+     * Closes (and flushes) all readers.
+     *
+     * @throws IOException If a file is locked and the reader therefore can not be closed.
+     */
     private void closeReaders() throws IOException {
-        BufferedReader[] readers = this.readers;
-
         for (BufferedReader reader : readers) {
-            if (reader != null) {
-                reader.close();
-            }
+            reader.close();
         }
-
     }
 }
