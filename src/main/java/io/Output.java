bdcf4bdfe6908aafcba81603f8f4bccc25d1e15c
Jakob L. Müller <me@jakob-l-m.de>
2024-03-21 20:10:32 +0100

    added method to store pINDs to disk

--- src/main/java/io/Output.java
+++ src/main/java/io/Output.java
@@ -2,8 +2,9 @@ package io;
 
 import runner.Config;
 import structures.Attribute;
-import structures.Candidates;
 import structures.Clock;
+import structures.PINDList;
+import structures.RelationMetadata;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
 
 public class Output {
 
@@ -24,37 +24,42 @@ public class Output {
     /**
      * Given the currently validated pINDs, a human-readable file will be created.
      *
-     * @param candidates The current Candidate class
-     * @param inputs The input files
+     * @param inputs     The input files
      * @param attributes The current attribute index
+     * @param layer The current layer. Needed to save the pINDs to layer-based files
      * @throws IOException If something goes wrong during output writing
      */
-    /*
-    public void storePINDs(Candidates candidates, List<RelationalInput> inputs, Attribute[] attributes) throws IOException {
-        if (candidates.current.isEmpty()) return;
+    public void storePINDs(RelationMetadata[] inputs, Attribute[] attributes, int layer) throws IOException {
 
-        BufferedWriter outputWriter = Files.newBufferedWriter(Path.of(this.resultFolder + File.separator + candidates.layer + "-ary_pINDs.txt"), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
-        for (int depId : candidates.current.keySet()) {
-            Attribute dependantAttribute = attributes[depId];
+        BufferedWriter outputWriter = Files.newBufferedWriter(Path.of(this.resultFolder + File.separator + layer + "-ary_pINDs.txt"), StandardOpenOption.TRUNCATE_EXISTING,
+                StandardOpenOption.CREATE);
+        for (Attribute dependantAttribute : attributes) {
+
+            if (dependantAttribute.getReferenced() == null) {
+                continue;
+            }
+
             outputWriter.write('(');
             for (int i = 0; i < dependantAttribute.getContainedColumns().length; i++) {
-                RelationalInput input = inputs.get(dependantAttribute.getRelationId());
+                RelationMetadata input = inputs[dependantAttribute.getRelationId()];
                 outputWriter.write(input.relationName);
                 outputWriter.write('.');
-                outputWriter.write(input.headerLine[dependantAttribute.getContainedColumns()[i]]);
+                outputWriter.write(input.columnNames[dependantAttribute.getContainedColumns()[i]]);
                 if (i != dependantAttribute.getContainedColumns().length - 1) {
                     outputWriter.write(',');
                 }
             }
             outputWriter.write(") <=");
-            for (int refId : candidates.current.get(depId).keySet()) {
+            PINDList.PINDIterator it = dependantAttribute.getReferenced().elementIterator();
+            while (it.hasNext()) {
+                int refId = it.next().referencedId;
                 Attribute referredAttribute = attributes[refId];
                 outputWriter.write(" (");
                 for (int i = 0; i < referredAttribute.getContainedColumns().length; i++) {
-                    RelationalInput input = inputs.get(referredAttribute.getRelationId());
+                    RelationMetadata input = inputs[referredAttribute.getRelationId()];
                     outputWriter.write(input.relationName);
                     outputWriter.write('.');
-                    outputWriter.write(input.headerLine[referredAttribute.getContainedColumns()[i]]);
+                    outputWriter.write(input.columnNames[referredAttribute.getContainedColumns()[i]]);
                     if (i != referredAttribute.getContainedColumns().length - 1) {
                         outputWriter.write(',');
                     }
@@ -63,14 +68,10 @@ public class Output {
             }
             outputWriter.newLine();
         }
-        outputWriter.flush();
         outputWriter.close();
     }
 
-     */
-
     /**
-     *
      * @param config
      */
     public void storeMetadata(Config config, Clock clock) {
